668ec153dcc2ad6fc011f71a470515338131c527
Edwin Diaz <jdiaz@playdigitalconsulting.com>
2023-01-05 11:00:15 -0500

    Frontend de servicios de Inventario

---
+++ Gust.Core/Controllers/EquipoController.cs
@@ -0,0 +1,57 @@
+﻿using Gust.Core.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Gust.Core.Data;
+using Gust.Core.Areas.Identity.Data.Forms;
+using Microsoft.AspNetCore.Identity;
+using Gust.Core.Areas.Identity.Data;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gust.Core.Controllers
+{
+    public class EquipoController : Controller
+    {
+        private readonly ILogger<EquipoController> _logger;
+        private readonly GustCoreContext _context;
+        private readonly UserManager<GustCoreUser> _userManager;
+
+        public EquipoController(
+            ILogger<EquipoController> logger,
+            GustCoreContext context,
+            UserManager<GustCoreUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        //Metodos de Navegacion
+        public IActionResult Inventario()
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+
+        public IActionResult Actualizar()
+        {
+            return View();
+        }
+    }
+}
