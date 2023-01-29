f5e6bdc0c0385eeb6473c7d853ff796026cd4770
Raul Ramirez <raul.ramirez@utp.ac.pa>
2023-01-29 13:22:03 -0500

    último cambio a la BD.

--- Gust.Core/Areas/Identity/Data/ModelBuilderExtensions.cs
+++ Gust.Core/Areas/Identity/Data/ModelBuilderExtensions.cs
@@ -12,7 +12,7 @@ namespace Gust.Core.Areas.Identity.Data
             List<IdentityRole> roles = new()
             {
                 new IdentityRole{ Name = "Administrador", NormalizedName = "ADMINISTRADOR".ToUpper()},
-                new IdentityRole{ Name = "Tec", NormalizedName = "TEC".ToUpper()}
+                new IdentityRole{ Name = "Tecnico", NormalizedName = "TEC".ToUpper()}
             };
 
             modelBuilder.Entity<IdentityRole>().HasData(roles);
@@ -23,16 +23,16 @@ namespace Gust.Core.Areas.Identity.Data
             {
                 new GustCoreUser
                 {
-                    UserName = "luis.villalaz1@utp.ac.pa",
-                    NormalizedUserName = "LUIS.VILLALAZ1@UTP.AC.PA".ToUpper(),
-                    Email = "luis.villalaz1@utp.ac.pa",
-                    NormalizedEmail = "LUIS.VILLALAZ1@UTP.AC.PA".ToUpper(),
+                    UserName = "omar.carvajal@utp.ac.pa",
+                    NormalizedUserName = "OMAR.CARVAJAL@UTP.AC.PA".ToUpper(),
+                    Email = "omar.carvajal@utp.ac.pa",
+                    NormalizedEmail = "OMAR.CARVAJAL@UTP.AC.PA".ToUpper(),
                     EmailConfirmed = true
                 }
             };
 
             var passwordHasher = new PasswordHasher<GustCoreUser>();
-            users[0].PasswordHash = passwordHasher.HashPassword(users[0], "Passgust22");
+            users[0].PasswordHash = passwordHasher.HashPassword(users[0], "GUST2023@");
 
             modelBuilder.Entity<GustCoreUser>().HasData(users);
 
@@ -47,45 +47,205 @@ namespace Gust.Core.Areas.Identity.Data
                 new IdentityUserRole<string>
                 {
                     UserId = users[0].Id,
-                    RoleId = roles.First(r => r.Name == "Tec").Id
+                    RoleId = roles.First(r => r.Name == "Tecnico").Id
                 }
             };
 
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(userRoles);
 
-            // Equipos
-            List<Equipo> equipos = new()
+            // Laboratorios
+            List<Laboratorio> laboratorios = new()
             {
-                new Equipo
-                {
+                new Laboratorio{
                     Id = 1,
-                    CodigoBienesPatrimoniales = "E_PRUEBA_1",
-                    Identificador = "EP1",
-                    NombreEquipo = "Equipo Prueba 1",
-                    Modelo = "Modelo1",
-                    Activo = true
+                    Codigo = "3-401",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
                 },
-                new Equipo
-                {
+                new Laboratorio{
                     Id = 2,
-                    CodigoBienesPatrimoniales = "E_PRUEBA_2",
-                    Identificador = "EP2",
-                    NombreEquipo = "Equipo Prueba 2",
-                    Modelo = "Modelo2",
-                    Activo = true
+                    Codigo = "3-402",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
                 },
-                new Equipo
-                {
-                    Id = 3,
-                    CodigoBienesPatrimoniales = "E_PRUEBA_3",
-                    Identificador = "EP3",
-                    NombreEquipo = "Equipo Prueba 3",
-                    Modelo = "Modelo3",
-                    Activo = true
+                new Laboratorio{Id = 3,
+                    Codigo = "3-403",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 4,
+                    Codigo = "3-404",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 5,
+                    Codigo = "3-405",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 6,
+                    Codigo = "3-406",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 7,
+                    Codigo = "3-407",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 8,
+                    Codigo = "3-408",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 9,
+                    Codigo = "3-409",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 10,
+                    Codigo = "3-410",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 11,
+                    Codigo = "3-BG01",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 12,
+                    Codigo = "3-411",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 13,
+                    Codigo = "3-412",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 14,
+                    Codigo = "3-413",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 15,
+                    Codigo = "3-414",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 16,
+                    Codigo = "3-415",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 17,
+                    Codigo = "3-416",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 18,
+                    Codigo = "3-417",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 19,
+                    Codigo = "3-418",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 20,
+                    Codigo = "3-419",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 21,
+                    Codigo = "3-420",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 22,
+                    Codigo = "3-421",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
+                },
+                new Laboratorio{Id = 23,
+                    Codigo = "3-422",
+                    Activo = true,
+                    CantidadComputadoras = 20,
+                    CantidadMesas = 20,
+                    CantidadSillas = 20
+
                 }
             };
 
-            modelBuilder.Entity<Equipo>().HasData(equipos);
+            modelBuilder.Entity<Laboratorio>().HasData(laboratorios);
+
         }
     }
 }
